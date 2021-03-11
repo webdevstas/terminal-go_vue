7b0b362ac247063dd276e6e0e0968bbd94a9e5b5
webdevstas <spirin.stas@mail.ru>
2021-03-11 11:38:29 +0300

    добавил роутер

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import gsap from './plugins/gsap'
+import router from './router'
 
 createApp(App)
+  .use(router)
   .use(store)
   .use(gsap)
   .mount('#app')
